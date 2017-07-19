054479b61893d6588be5ccd9af0b23773aeb699b
markDeveloping <mark.developing@gmail.com>
2017-07-19 19:22:36 +0100

    Added examples to cover eacr each senario.

--- app/routes.js
+++ app/routes.js
@@ -3,17 +3,24 @@ var router = express.Router()
 
 // Route index page
 router.get('/', function (req, res) {
-  res.render('index')
+  res.render('reserved')
 })
 
 // add your routes here
 router.get('/reserved', function (req, res) {
-  res.render('index')
+  res.render('reserved')
 })
 
 router.get('/non-reserved', function (req, res) {
   res.render('non_reserved')
 })
 
+router.get('/reserved_plus', function (req, res) {
+  res.render('reserved_plus')
+})
+
+router.get('/non-reserved', function (req, res) {
+  res.render('non-reserved_plus')
+})
 
 module.exports = router
